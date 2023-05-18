0809b3995942b3f02e6ef6c6872502fd933bc7fd
RobinLou <robinlu96@gmail.com>
2023-05-18 10:53:13 -0400

    add home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+
+  @Output('ExploreMe')
+  ExploreMe = new EventEmitter();
+
+  startTadaAnimation = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+        setInterval(() => {
+      this.startTadaAnimation = !this.startTadaAnimation;
+    }, 3000);
+  }
+
+  onExploreMeClick() {
+    this.ExploreMe.emit();
+  }
+}
